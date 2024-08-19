8a2ff12b499ab5b48ae6f1a2f2c848a51b3ea59e
Rahi Ahmed <87031439+Rahi-Ahmed@users.noreply.github.com>
2024-08-18 21:13:40 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,81 +1,91 @@
 package OnlineShoppingSystem;
 
+// Abstract class representing a User
 public abstract class User {
 
-	// Attributes for user
-	private String name;
-	private String userName;
-	private String password;
-	private String userType;
+    // Attributes for user
+    private String name;
+    private String userName;
+    private String password;
+    private String userType;
 
-	// Constructor
-	public User(String name, String userName, String password, String userType) {
-		this.name = name;
-		this.userName = userName;
-		this.password = password;
-		this.userType = userType;
-	}
+    // Constructor
+    public User(String name, String userName, String password, String userType) {
+        this.name = name;
+        this.userName = userName;
+        this.password = password;
+        this.userType = userType;
+    }
 
-	// Getter and setter method
-	public String getName() {
-		return name;
-	}
+    // Getter and setter methods for name
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getUserName() {
-		return userName;
-	}
+    // Getter and setter methods for userName
+    public String getUserName() {
+        return userName;
+    }
 
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    // Getter and setter methods for password
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getUserType() {
-		return userName;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public void setUserType(String userName) {
-		this.userName = userName;
-	}
+    // Getter and setter methods for userType
+    public String getUserType() {
+        return userType;
+    }
 
-	@Override
-	public String toString() {
-		return "User [Name=" + name + ", Username=" + userName + ", User Type=" + userType + "]";
-	}
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    // Override the toString method to provide a string representation of the User object
+    @Override
+    public String toString() {
+        return "User [Name=" + name + ", Username=" + userName + ", User Type=" + userType + "]";
+    }
 }
 
-// Admin class extending user class
+// Admin class extending User class
 class Admin extends User {
 
-	public Admin(String name, String userName, String password, String userType) {
-		super(name, userName, password, userType);
-	}
+    // Constructor
+    public Admin(String name, String userName, String password, String userType) {
+        super(name, userName, password, userType);
+    }
 
-	@Override
-	public String toString() {
-		return "Admin [Name=" + getName() + ", Username=" + getUserName() + "]";
-	}
+    // Override the toString method to provide a string representation of the Admin object
+    @Override
+    public String toString() {
+        return "Admin [Name=" + getName() + ", Username=" + getUserName() + "]";
+    }
 }
 
-//Customer class extending user class
+// Customer class extending User class
 class Customer extends User {
 
-	public Customer(String name, String userName, String password, String userType) {
-		super(name, userName, password, userType);
-	}
+    // Constructor
+    public Customer(String name, String userName, String password, String userType) {
+        super(name, userName, password, userType);
+    }
 
-	@Override
-	public String toString() {
-		return "Customer [Name=" + getName() + ", Username=" + getUserName() + "]";
-	}
+    // Override the toString method to provide a string representation of the Customer object
+    @Override
+    public String toString() {
+        return "Customer [Name=" + getName() + ", Username=" + getUserName() + "]";
+    }
 }
