e7f4ebc8430b42137685f155380861ec3c3419dc
Rahi Ahmed <87031439+Rahi-Ahmed@users.noreply.github.com>
2024-08-18 21:12:28 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,14 @@
+package OnlineShoppingSystem;
+
+import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+ 
+		// Instance of store interface
 		StoreInterface storeInterface = new StoreInterface();
-                storeInterface.showMainMenu();
+		storeInterface.showMainMenu();
 
 	}
 
